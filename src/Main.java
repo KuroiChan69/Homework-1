01fbd2aa5103fc5049480a9d981f8d3b91c2dc81
KuroiChan69 <nikita.papshev@gmail.com>
2024-04-01 03:01:19 +0700

    Added TASK 6

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         var dog = 10.0;
         var cat = 20.0;
         var paper = 763789;
+        System.out.println("Задание 1");
         System.out.println("кошка "+cat);
         System.out.println("собака "+dog);
         System.out.println("бумага "+paper);
@@ -11,22 +12,26 @@ public class Main {
         dog=dog+a;
         cat=cat+a;
         paper=paper+a;
+        System.out.println("Задание 2");
         System.out.println("кошка +4 "+cat);
         System.out.println("собака +4"+dog);
         System.out.println("бумага +4"+paper);
        dog=dog-3.5;
        cat =cat-1.6;
         paper=paper-7639;
+        System.out.println("Задание 3");
         System.out.println("у кошки отняли "+cat);
         System.out.println("у собаки отняли "+dog);
         System.out.println("из бумаги вычли "+paper);
         var friend=19;
+        System.out.println("Задание 4");
         System.out.println(friend);
         friend=friend+2;
         System.out.println(friend);
         friend=friend/7;
         System.out.println(friend);
         var frog=3.5;
+        System.out.println("Задание 5");
         System.out.println("лягушка"+frog);
         frog=frog*10;
         System.out.println("умножилии"+frog);
@@ -34,6 +39,11 @@ public class Main {
         System.out.println("поделили"+frog);
         frog=frog+4;
         System.out.println("добавили 4 "+frog);
+        System.out.println("Задание 6");
+        var fighter1=78.2;
+        var fighter2=82.7;
+        System.out.println("общая масса"+(fighter1+fighter2));
+        System.out.println("разница масс"+(fighter2-fighter1));
     }
 
 
