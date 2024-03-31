e335c35345dee3d855528ebbba06823a20f60fab
KuroiChan69 <nikita.papshev@gmail.com>
2024-04-01 02:38:44 +0700

    added friend

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 10;
-        var cat = 20;
+                Object var;
+        var dog = 10.0;
+        var cat = 20.0;
         var paper = 763789;
-        Object var;
-        dog=dog+4;
-        cat=cat+4;
-        paper=paper+4;
         System.out.println("кошка "+cat);
         System.out.println("собака "+dog);
-        System.out.println("бумага "+paper);}
+        System.out.println("бумага "+paper);
+        var a=4;
+        dog=dog+a;
+        cat=cat+a;
+        paper=paper+a;
+        System.out.println("кошка +4 "+cat);
+        System.out.println("собака +4"+dog);
+        System.out.println("бумага +4"+paper);
+       dog=dog-3.5;
+       cat =cat-1.6;
+        paper=paper-7639;
+        System.out.println("у кошки отняли "+cat);
+        System.out.println("у собаки отняли "+dog);
+        System.out.println("из бумаги вычли "+paper);
+        var friend=19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+    }
 
 
 }
