b03916a6ee441270117cbdd3b1a8541995411559
KuroiChan69 <nikita.papshev@gmail.com>
2024-04-01 00:42:23 +0700

    added +4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Object var;
-        var dog = 10;
-        var cat = 20;
-        var paper = 763789;
-        System.out.println("кошка "+cat);
-        System.out.println("собака "+dog);
-        System.out.println("бумага "+paper);
+        var dog = 10+4;
+        var cat = 20+4;
+        var paper = 763789+4;
+        System.out.println("кошка +4"+cat);
+        System.out.println("собака +4"+dog);
+        System.out.println("бумага +4"+paper);
 
     }
 
