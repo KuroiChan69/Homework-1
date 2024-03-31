f30fdeb2752c87e6eafe3478d2e3dace657b015c
KuroiChan69 <nikita.papshev@gmail.com>
2024-04-01 00:13:56 +0700

    added new variables and displayed values

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Object var;
+        var dog = 10;
+        var cat = 20;
+        var paper = 763789;
+                System.out.println(cat);
+        System.out.println(dog);
+        System.out.println(paper); }
 
-        System.out.println("Привет мир!");
-        System.out.println("Hello world!"); }
 }
